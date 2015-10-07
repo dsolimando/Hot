b5e3b2fa7712b230b0606941ca0eed7f577e14d2
Damien Solimando <dsolimando@gmail.com>
2015-10-07 14:08:47 +0200

    unneeded imports

--- hot-framework/src/main/java/be/solidx/hot/shows/spring/ClosureRequestMappingHandlerMapping.java
+++ hot-framework/src/main/java/be/solidx/hot/shows/spring/ClosureRequestMappingHandlerMapping.java
@@ -4,11 +4,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
