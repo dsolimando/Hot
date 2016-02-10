91f6c7e187097bf2b7dfc976c59bd711aa684c29
Damien Solimando <dsolimando@gmail.com>
2016-02-10 14:39:13 +0100

    parsing of form params in request body

--- hot-framework/src/main/java/be/solidx/hot/utils/AbstractHttpDataDeserializer.java
+++ hot-framework/src/main/java/be/solidx/hot/utils/AbstractHttpDataDeserializer.java
@@ -1,9 +1,12 @@
 package be.solidx.hot.utils;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.nio.charset.Charset;
 import java.util.Map;
 
+import org.apache.commons.fileupload.ParameterParser;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +17,8 @@ public abstract class AbstractHttpDataDeserializer implements HttpDataDeserializ
 	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractHttpDataDeserializer.class);
 	
 	ObjectMapper objectMapper;
+	
+	ParameterParser parameterParser = new ParameterParser();
 	
 	public AbstractHttpDataDeserializer(ObjectMapper objectMapper) {
 		this.objectMapper = objectMapper;
@@ -26,12 +31,14 @@ public abstract class AbstractHttpDataDeserializer implements HttpDataDeserializ
 		if (LOGGER.isDebugEnabled())
 			LOGGER.debug("Content-type: "+contentType);
 		
+		Charset charset = ct.getCharSet() == null?Charset.forName("UTF-8"):ct.getCharSet();
+		
 		// Subtypes arre used because of possible encoding definitions
 		if (ct.getSubtype().equals(MediaType.APPLICATION_OCTET_STREAM.getSubtype())) {
 			return data;
 		} else if (ct.getSubtype().equals(MediaType.APPLICATION_JSON.getSubtype())) {
 			try {
-				return toJSON(data);
+				return fromJSON(data);
 			} catch (IOException e) {
 				throw new RuntimeException(e.getMessage(), e.getCause());
 			}
@@ -41,15 +48,28 @@ public abstract class AbstractHttpDataDeserializer implements HttpDataDeserializ
 			} catch (Exception e) {
 				throw new RuntimeException(e.getMessage(), e.getCause());
 			}
-		} else {
+		} else if (ct.getSubtype().equals(MediaType.APPLICATION_FORM_URLENCODED.getSubtype())) {
+			String decoded;
+			try {
+				decoded = URLDecoder.decode(new String(data,charset),charset.toString());
+			} catch (Exception e) {
+				throw new RuntimeException(e.getMessage(), e.getCause());
+			}
+			return fromFormUrlEncoded(decoded);
+		}
+		else {
 			return new String(data,ct.getCharSet() == null?Charset.forName("UTF-8"):ct.getCharSet());
 		}
 	}
 	
 	protected abstract Object toXML(byte[] data, Charset charset);
 
-	protected Map<?, ?> toJSON(byte[] data) throws IOException {
+	protected Map<?, ?> fromJSON(byte[] data) throws IOException {
 		return objectMapper.readValue(data, Map.class);
+	}
+	
+	protected Map<?, ?> fromFormUrlEncoded(String data) {
+		return parameterParser.parse(data,'&');
 	}
 	
 	public static class DeserializationException extends RuntimeException {
