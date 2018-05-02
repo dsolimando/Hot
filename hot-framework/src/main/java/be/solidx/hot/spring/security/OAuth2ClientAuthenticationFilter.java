9ef56e57081d24efee7771e6caa14a1c42c70820
Solimando Damien <dsolimando@gmail.com>
2018-05-02 21:47:52 +0200

    Last version of spring social

--- hot-framework/src/main/java/be/solidx/hot/spring/security/OAuth2ClientAuthenticationFilter.java
+++ hot-framework/src/main/java/be/solidx/hot/spring/security/OAuth2ClientAuthenticationFilter.java
@@ -206,7 +206,7 @@ public class OAuth2ClientAuthenticationFilter extends GenericFilterBean {
 	}
 	
 	private void addSignInAttempt(HttpSession session, Connection<?> connection) {
-		session.setAttribute(ProviderSignInAttempt.SESSION_ATTRIBUTE, new ProviderSignInAttempt(connection, authServiceLocator, usersConnectionRepository));
+		session.setAttribute(ProviderSignInAttempt.SESSION_ATTRIBUTE, new ProviderSignInAttempt(connection));
 	}
 
 	private static final String DEFAULT_FAILURE_URL = "/signin";
---
+++ hot-framework/src/test/java/be/solidx/hot/test/async/TestAsync.java
@@ -0,0 +1,69 @@
+package be.solidx.hot.test.async;
+
+import com.ea.async.Async;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Function;
+
+/**
+ * Created by dsolimando on 13/04/2018.
+ */
+public class TestAsync {
+
+    static {
+        Async.init();
+    }
+
+    private static class AsyncTask {
+
+        CompletableFuture<String> run(Function<Void,String> r) {
+
+            CompletableFuture<String> cf = new CompletableFuture<>();
+
+            new Thread(() -> {
+                try {
+                    Thread.sleep(1000);
+
+                    cf.complete(r.apply(null));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            return cf;
+        }
+    }
+
+    private static class AsyncTest {
+
+        CompletableFuture test(CompletableFuture cf) {
+            System.out.println("1");
+            String s =  Async.await(cf);
+            System.out.println(s);
+            System.out.println("2");
+            return CompletableFuture.completedFuture(s);
+        }
+    }
+
+    @Test
+    public void testAsync() throws ExecutionException, InterruptedException {
+
+        ExecutorService es = Executors.newFixedThreadPool(1);
+
+
+        es.execute(() -> {
+            CompletableFuture<String> cf = new AsyncTest().test(new AsyncTask().run(a -> "Hello"));
+        });
+        es.execute(() -> {
+            CompletableFuture<String> cf = new AsyncTest().test(new AsyncTask().run(a -> "Hello"));
+        });
+
+        Thread.sleep(10000);
+        //Assert.assertEquals("Hello",cf.get());
+    }
+}
